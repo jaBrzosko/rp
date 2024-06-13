e6dfd8241be569bd44d784e2c108f117ad9e71aa
Bartosz Dybowski <01161292@pw.edu.pl>
2024-06-13 11:37:21 +0200

    Fixed map drawing

--- clientApp/weather-app/src/App.js
+++ clientApp/weather-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import DatePicker from 'react-datepicker';
 import axios from 'axios';
 import './App.css';
@@ -22,12 +22,10 @@ function App() {
   const [maxDaysInCurrentMonth, setMaxDaysInCurrentMonth] = useState(0)
   const [currentDay, setCurrentDay] = useState(0);
   const [currentHour, setCurrentHour] = useState(0);
-  const [overlayPanelsColor, setOverlayPanelsColor] = useState(initialColors);
   const [dataReady, setDataReady] = useState(false)
   const [isAnimationPlaying, setIsAnimationPlaying] = useState(false)
-  const [currentIterationConst, setCurrentIterationConst] = useState(0)
-  var currentIteration = 0;
-  var stopAnimation = false;
+  const intervalRef = useRef(null);
+  const currentIterationRef = useRef(0)
 
   // temperatures
   const [northWestTemperature, setNorthWestTemperature] = useState(null)
@@ -36,25 +34,151 @@ function App() {
   const [southEastTemperature, setSouthEastTemperature] = useState(null)
   const [centerTemperature, setCenterTemperature] = useState(null)
 
-  const increaseIteration = () => {
-    currentIteration = currentIteration + 1 <= maxDaysInCurrentMonth ? currentIteration + 1 : currentIteration
-    setCurrentIterationConst(currentIteration)
-  }
-
-  const decreaseIteration = () => {
-    currentIteration = currentIteration - 1 >= maxDaysInCurrentMonth ? currentIteration - 1 : currentIteration
-    setCurrentIterationConst(currentIteration)
-  }
-
   useEffect(() => {
-    overlayPanelsColor.forEach((element, i) => {
-      const index = i + 1;
-      document.documentElement.style.setProperty(`--bottom-left-color-${index}`, element.topRightColor);
-      document.documentElement.style.setProperty(`--top-right-color-${index}`, element.bottomLeftColor);
-      document.documentElement.style.setProperty(`--bottom-right-color-${index}`, element.bottomRightColor);
-      document.documentElement.style.setProperty(`--top-left-color-${index}`, element.topLeftColor);
-    });
-  }, [overlayPanelsColor]);
+
+    const calculateColors = () => {
+      const calculatedColors = []
+      for (let i = 0; i < 12; ++i) {
+        calculatedColors.push([])
+      }
+      var dataIndex = 0;
+      const defaultColor = "rgba(0, 0, 0, 1)"
+      for (let i = 0; i < 12; ++i) {
+        for (let j = 0; j < 12; ++j) {
+          if (i === 0 || i === 11 || j === 0 || j === 11) {
+            calculatedColors[i].push(defaultColor)
+          }
+          else {
+            const temperatureString = data.points[dataIndex].date_temperature[currentIterationRef.current].temperature;
+            var calculatedColor = getColorForTemperature(parseFloat(temperatureString))
+            calculatedColors[i].push(calculatedColor)
+            if (dataIndex === 25) {
+              setNorthWestTemperature(temperatureString)
+            }
+            else if (dataIndex === 41) {
+              setNorthEastTemperature(temperatureString)
+            }
+            else if (dataIndex === 57) {
+              setSouthWestTemperature(temperatureString)
+            }
+            else if (dataIndex === 61) {
+              setCenterTemperature(temperatureString)
+            }
+            else if (dataIndex === 95) {
+              setSouthEastTemperature(temperatureString)
+            }
+            dataIndex++;
+          }
+        }
+      }
+      var colors = []
+      for (let i = 0; i < 11; ++i) {
+        for (let j = 0; j < 11; ++j) {
+          colors.push({
+            bottomLeftColor: calculatedColors[i][j + 1],
+            bottomRightColor: calculatedColors[i + 1][j + 1],
+            topLeftColor: calculatedColors[i][j],
+            topRightColor: calculatedColors[i + 1][j]
+          });
+        }
+      }
+      return colors;
+    }
+
+    const interpolateColor = (color1, color2, factor) => {
+      const result = color1.slice();
+      for (let i = 0; i < 3; i++) {
+        result[i] = Math.round(result[i] + factor * (color2[i] - color1[i]));
+      }
+      return result;
+    };
+
+    const getColorForTemperature = (temperature) => {
+      const colors = [
+        { temperature: -25, color: [0, 0, 255] },    // Deep blue
+        { temperature: -16, color: [128, 128, 255] },    // Blue
+        { temperature: -8, color: [1, 255, 255] },    // Light blue
+        { temperature: 0, color: [255, 255, 255] },  // White
+        { temperature: 8, color: [255, 242, 178] },     // Yellow
+        { temperature: 16, color: [255, 103, 0] },     // Orange
+        { temperature: 25, color: [255, 0, 0] }      // Red
+      ];
+
+      if (temperature <= -20) return 'rgb(0, 0, 255, 0.5)';
+      if (temperature >= 20) return 'rgb(255, 0, 0, 0.5)';
+
+      let lower, upper;
+      for (let i = 0; i < colors.length - 1; i++) {
+        if (temperature >= colors[i].temperature && temperature <= colors[i + 1].temperature) {
+          lower = colors[i];
+          upper = colors[i + 1];
+          break;
+        }
+      }
+
+      const range = upper.temperature - lower.temperature;
+      const factor = (temperature - lower.temperature) / range;
+
+      const color = interpolateColor(lower.color, upper.color, factor);
+
+      return `rgb(${color[0]}, ${color[1]}, ${color[2]}, 0.5)`;
+    };
+
+    if (isAnimationPlaying) {
+      intervalRef.current = setInterval(() => {
+        var end = false;
+
+        if (currentDay === maxDaysInCurrentMonth && currentHour === 23) {
+          end = true;
+        }
+
+        if (end) {
+          setIsAnimationPlaying(false);
+          clearInterval(intervalRef.current);
+          setCurrentDay(0)
+          setCurrentHour(0)
+          currentIterationRef.current = 0;
+        }
+        else {
+          var colors = calculateColors()
+          colors.forEach((element, i) => {
+            const index = i + 1;
+            document.documentElement.style.setProperty(`--bottom-left-color-${index}`, element.topRightColor);
+            document.documentElement.style.setProperty(`--top-right-color-${index}`, element.bottomLeftColor);
+            document.documentElement.style.setProperty(`--bottom-right-color-${index}`, element.bottomRightColor);
+            document.documentElement.style.setProperty(`--top-left-color-${index}`, element.topLeftColor);
+          });
+
+          setCurrentHour((prevHour) => {
+            return prevHour !== 23 ? prevHour + 1 : 0;
+          });
+  
+          setCurrentDay((prevDay) => {
+            return currentHour === 23 ? prevDay + 1 : prevDay;
+          });
+
+          currentIterationRef.current += 1
+        }
+      }, 100);
+    }
+
+    return () => clearInterval(intervalRef.current);
+  }, [isAnimationPlaying, currentDay, currentHour, data.points, maxDaysInCurrentMonth]);
+
+
+  const stopAnimation = () => {
+    setIsAnimationPlaying(false);
+    clearInterval(intervalRef.current);
+  };
+
+
+  const endAnimation = () => {
+    setIsAnimationPlaying(false);
+    clearInterval(intervalRef.current);
+    setCurrentDay(0)
+    setCurrentHour(0)
+    currentIterationRef.current = 0
+  };
 
   const fetchData = async (year, month) => {
     try {
@@ -81,99 +205,20 @@ function App() {
     fetchData(year, month)
   }
 
-  const handleLeftArrowClick = () => {
-    if (maxDaysInCurrentMonth === 0 || !dataReady) return;
-    if (currentDay === 0 && currentHour === 0) return;
-    if (currentHour === 0) {
-      var newCurrentDay = currentDay - 1;
-      setCurrentDay(newCurrentDay)
-      setCurrentHour(23)
-    }
-    else {
-      var newCurrentHour = currentHour - 1
-      setCurrentHour(newCurrentHour)
-    }
-    decreaseIteration()
-  }
-
-  const handleRightArrowClick = () => {
-    if (maxDaysInCurrentMonth === 0 || !dataReady) return;
-    if (currentDay === maxDaysInCurrentMonth && currentHour === 23) return;
-    if (currentHour === 23) {
-      var newCurrentDay = currentDay + 1;
-      setCurrentDay(newCurrentDay)
-      setCurrentHour(0)
-    }
-    else {
-      var newCurrentHour = currentHour + 1;
-      setCurrentHour(newCurrentHour)
-    }
-    increaseIteration()
-  }
-
-  const handleStartToggleButton = () => {
-    if (isAnimationPlaying || data === null || !dataReady) return;
-    setIsAnimationPlaying(true)
-    stopAnimation = false;
-    playAnimation()
-  }
-
-  const handleStopToggleButton = () => {
-    if (!isAnimationPlaying) return;
-    setIsAnimationPlaying(false)
-    stopAnimation = true
-  }
-
-  const handleRestartToggleButton = () => {
-      setCurrentDay(1)
-      setCurrentHour(0)
-      setCurrentIterationConst(0)
-      currentIteration = 0
-    if (isAnimationPlaying === false) {
-      handleStartToggleButton()
-    }
-  }
-
-  function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
-  const playAnimation = async () => {
-    var end = false;
-    while (!stopAnimation && !end) {
-      calculateColors();
-  
-      if (currentDay === maxDaysInCurrentMonth && currentHour === 23) {
-        end = true;
-      } else {
-          setCurrentHour(prevHour => {
-            if (prevHour === 23) {
-              setCurrentDay(prevDay => prevDay + 1);
-              return 0;
-            } else {
-              return prevHour + 1;
-            }
-          });
-      }
-      increaseIteration()
-      await sleep(1000)
-    }
-  };
-
   const calculateColors = () => {
     const calculatedColors = []
-    for(let i = 0; i < 12; ++i) {
+    for (let i = 0; i < 12; ++i) {
       calculatedColors.push([])
     }
     var dataIndex = 0;
     const defaultColor = "rgba(0, 0, 0, 1)"
-    for(let i = 0; i < 12; ++i) {
+    for (let i = 0; i < 12; ++i) {
       for (let j = 0; j < 12; ++j) {
         if (i === 0 || i === 11 || j === 0 || j === 11) {
           calculatedColors[i].push(defaultColor)
         }
         else {
-          const temperatureString = data.points[dataIndex].date_temperature[currentIteration].temperature;
+          const temperatureString = data.points[dataIndex].date_temperature[currentIterationRef.current].temperature;
           var calculatedColor = getColorForTemperature(parseFloat(temperatureString))
           calculatedColors[i].push(calculatedColor)
           if (dataIndex === 25) {
@@ -199,14 +244,14 @@ function App() {
     for (let i = 0; i < 11; ++i) {
       for (let j = 0; j < 11; ++j) {
         colors.push({
-            bottomLeftColor: calculatedColors[i][j + 1],
-            bottomRightColor: calculatedColors[i + 1][j + 1],
-            topLeftColor: calculatedColors[i][j],
-            topRightColor: calculatedColors[i + 1][j]
-          });
-        }
+          bottomLeftColor: calculatedColors[i][j + 1],
+          bottomRightColor: calculatedColors[i + 1][j + 1],
+          topLeftColor: calculatedColors[i][j],
+          topRightColor: calculatedColors[i + 1][j]
+        });
       }
-    setOverlayPanelsColor(colors)
+    }
+    return colors;
   }
 
   const interpolateColor = (color1, color2, factor) => {
@@ -216,7 +261,7 @@ function App() {
     }
     return result;
   };
-  
+
   const getColorForTemperature = (temperature) => {
     const colors = [
       { temperature: -25, color: [0, 0, 255] },    // Deep blue
@@ -227,10 +272,10 @@ function App() {
       { temperature: 16, color: [255, 103, 0] },     // Orange
       { temperature: 25, color: [255, 0, 0] }      // Red
     ];
-  
+
     if (temperature <= -20) return 'rgb(0, 0, 255, 0.5)';
     if (temperature >= 20) return 'rgb(255, 0, 0, 0.5)';
-  
+
     let lower, upper;
     for (let i = 0; i < colors.length - 1; i++) {
       if (temperature >= colors[i].temperature && temperature <= colors[i + 1].temperature) {
@@ -239,14 +284,75 @@ function App() {
         break;
       }
     }
-  
+
     const range = upper.temperature - lower.temperature;
     const factor = (temperature - lower.temperature) / range;
-  
+
     const color = interpolateColor(lower.color, upper.color, factor);
-  
+
     return `rgb(${color[0]}, ${color[1]}, ${color[2]}, 0.5)`;
   };
+
+  const handleLeftArrowClick = () => {
+    if (maxDaysInCurrentMonth === 0 || !dataReady) return;
+    if (currentDay === 0 && currentHour === 0) return;
+    var colors = calculateColors()
+    colors.forEach((element, i) => {
+      const index = i + 1;
+      document.documentElement.style.setProperty(`--bottom-left-color-${index}`, element.topRightColor);
+      document.documentElement.style.setProperty(`--top-right-color-${index}`, element.bottomLeftColor);
+      document.documentElement.style.setProperty(`--bottom-right-color-${index}`, element.bottomRightColor);
+      document.documentElement.style.setProperty(`--top-left-color-${index}`, element.topLeftColor);
+    });
+
+    setCurrentHour((prevHour) => {
+      return prevHour !== 0 ? prevHour - 1 : 23;
+    });
+
+    setCurrentDay((prevDay) => {
+      return currentHour === 0 ? prevDay - 1 : prevDay;
+    });
+
+    currentIterationRef.current -= 1
+  }
+
+  const handleRightArrowClick = () => {
+    if (maxDaysInCurrentMonth === 0 || !dataReady) return;
+    if (currentDay === maxDaysInCurrentMonth && currentHour === 23) return;
+    var colors = calculateColors()
+    colors.forEach((element, i) => {
+      const index = i + 1;
+      document.documentElement.style.setProperty(`--bottom-left-color-${index}`, element.topRightColor);
+      document.documentElement.style.setProperty(`--top-right-color-${index}`, element.bottomLeftColor);
+      document.documentElement.style.setProperty(`--bottom-right-color-${index}`, element.bottomRightColor);
+      document.documentElement.style.setProperty(`--top-left-color-${index}`, element.topLeftColor);
+    });
+
+    setCurrentHour((prevHour) => {
+      return prevHour !== 23 ? prevHour + 1 : 0;
+    });
+
+    setCurrentDay((prevDay) => {
+      return currentHour === 23 ? prevDay + 1 : prevDay;
+    });
+
+    currentIterationRef.current += 1
+  }
+
+  const handleStartToggleButton = () => {
+    if (isAnimationPlaying || data === null || !dataReady) return;
+    setIsAnimationPlaying(true)
+  }
+
+  const handleStopToggleButton = () => {
+    stopAnimation()
+  }
+
+  const handleRestartToggleButton = () => {
+     endAnimation()
+     setCurrentDay(1)
+     setIsAnimationPlaying(true)
+  }
 
   const getDayString = () => {
     if (currentDay === 0 || currentMonth === 0) return "";
@@ -323,7 +429,7 @@ function App() {
         <div className="image">
           <img src="./Poland.png" alt="Poland" />
           <div className="overlay-grid">
-            {overlayPanelsColor.map((color, index) => (
+            {initialColors.map((color, index) => (
               <div
                 key={index}
                 className={`overlay-panel overlay-panel-${index}`}
